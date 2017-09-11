4b80a3769d63b473884763037ffd2bf11f2c18b7
2039 <2039@users.noreply.github.com>
2017-09-11 22:07:50 +0200

    Add files via upload

--- proj.py
+++ proj.py
@@ -1,139 +1,208 @@
 #!/usr/bin/python -i
-"""Prosjekt 1"""
+"""Project 1 / Problem 2"""
 
 import numpy as np
 import math as m
 import matplotlib.pyplot as plt
+from collections import namedtuple
+from numpy.random import choice
 
-#kjapp måte å konvertere fra streng til numpy-array
+#Quick way to convert from string to array
 to_vector = lambda string: np.array([float(x) for x in string.strip().split()])
 
-#kjapp måte å konvertere fra multi-streng til numpy-matrise
+#Quick way to convert form multiline string to matrix
 to_matrix = lambda string: np.matrix([[float(y) for y in x.split()] for x in string.strip().splitlines()])
 
-#antall inndelte høydepartier
+#Number of altitude sections
 altitudes = 50
 
-#Sannsynlighetsfordelingen for lav risiko/høy risiko ved bunn
-P_bottom = to_vector("0.99 0.01")
+#A (modified, see next comment) namedtuple for the distributions
+Risks = namedtuple("Risks", "low high")
 
-#Overgangssannsynlighet til neste høydeparti
+#The functionality of __getitem__ is changed from nt.key to nt["key"]
+getitem = lambda self, key: self._asdict()[key]
+Risks.__getitem__ = getitem
+
+#The probability distribution for low risk/high risk at the bottom
+bottom_state = Risks(*(to_vector("0.99 0.01")))
+
+#Transition probability matrix for the next section
 P_transition = to_matrix("""
 0.95 0.05
 0.00 1.00
 """)
 
-#Mengde med overgangspotenser; fra 0 til 49.
+#A list of powers of the transition matrix; from 0 to (49), for speed.
 P_transitions = [P_transition ** altitude for altitude in range(altitudes)]
+
+#probability distribution at altitude
+def P(X, **sensor):
+	"""Markov probability
+	This function uses bottom_state and P_transition to calculate
+	P(X = altitude), or P(X = altitude, high = sensor_altitude), if
+	the **sensor input is given.
+
+	The return value i the probability distribution at altitude X,
+	this is given as a (modified) namedtuple: (low: low_val, high: high_val).
+
+	Example output:
+	(low: 0.95, high: 0.05)
+	"""
+	altitude = X #alias
+	if not sensor:
+		#A1 turns [[matrix]] to [list], which is more easily accessible
+		return Risks(*(bottom_state @ P_transitions[altitude]).A1)
+
+	#sensor exists = conditional probability
+	sensor_kw, sensor_altitude = sensor.popitem()
+	sensor_state = {"low":[1,0],"high":[0,1]}[sensor_kw]
+	distance_from_sensor = abs(sensor_altitude - altitude)
+
+	#If X is above sensor altitude, we can discard of bottom_state
+	if sensor_altitude <= altitude:
+		return Risks(*(sensor_state @ P_transitions[distance_from_sensor]).A1)
+
+	#Otherwise we need take care of both sensor_state and bottom_state
+	elif sensor_altitude > altitude:
+		return Risks(
+		#P(altitude = low | sensor = state)
+		P(X = altitude)["low"]
+		* P(X = sensor_altitude, low = altitude)[sensor_kw]
+		/(
+		P(X = altitude)["low"]
+		* P(X = sensor_altitude, low = altitude)[sensor_kw]
+		+ P(X = altitude)["high"]
+		* P(X = sensor_altitude, high = altitude)[sensor_kw]
+		)
+		,
+		#P(altitude = high | sensor = state)
+		P(X = altitude)["high"] *
+		P(X = sensor_altitude, high = altitude)[sensor_kw]
+		/(
+		P(X = altitude)["low"]
+		* P(X = sensor_altitude, low = altitude)[sensor_kw]
+		+ P(X = altitude)["high"]
+		* P(X = sensor_altitude, high = altitude)[sensor_kw])
+		)
 
 #A
 """
-Denne sier seg litt selv, viss ikke: les oppgaveteksten på nytt.
+Calculates the probability distribution for each altitude
+Then fetches the low risks.
+This can be done in a single line, but we need the high risks for question d).
 """
-altitude_risks = [(P_bottom @ P_transition).A1 for P_transition in P_transitions]
+#A1 turns [[matrix]] to [list], which is more easily accessible
+altitude_risks = [P(X = altitude) for altitude in range(altitudes)]
+low_risks = [risk["low"] for risk in altitude_risks]
 
-#plt.imshow(altitude_risks.T, cmap='pink', interpolation='nearest')
-#y_values = ["Low risk", "High risk"]
-#plt.yticks([0, 1], y_values)
-#plt.title("Risk given altitude")
-#plt.show()
+#plot
+plt.title("Probability of low risk given altitude")
+plt.xlabel("altitude"); plt.ylabel("probability")
+plt.bar(range(altitudes), low_risks)
+plt.show()
 
 #B
 """
-Her simulerer vi jernbanen 25 ganger og samler gjennomsnittet.
-numpy.choice velger mellom 0 og 1 utifra fordelingen i p.
-Kan effektiviseres fordi man ikke lenger har noe valg etter høyrisiko-soner.
+We simulate 25 different railroad scenarios.
+We use this to make an average risk prospect.
 """
-realizations = 25
-#Vi gjør klar lista så vi kan legge til direkte istedenfor å bruke append().
-average_risks = np.empty([altitudes, 2])
-for realization in range(realizations):
-	current_state = np.random.choice([0, 1], p = P_bottom) #first state
-	average_risks[0][current_state] += 1/realizations
-	for altitude in range(1, altitudes):
-		state_transition = np.ravel(P_transition[current_state])
-		current_state = np.random.choice([0, 1], p = state_transition)
-		average_risks[altitude][current_state] += 1/realizations
 
-#plt.imshow(average_risks.T, cmap='pink', interpolation='nearest')
-#plt.show()
+#simulation of a railroad
+def realization():
+	"""Runs a simulation of a hypothetical railroad, and
+	returns a list of the altitudes assigned a binary value
+	depending on their risk state: 1 = low, 0 = high.
+
+	Example output:
+	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0]
+	"""
+
+	#initialize empty list
+	risk_at_altitudes = [None for altitude in range(altitudes)]
+
+	#This chooses 0 or 1 depending on the distribution of bottom_state
+	#0 and 1 refers to state 1 and 2, respectively
+	current_state = choice([0, 1], p = bottom_state)
+	risk_at_altitudes[0] = 1 - current_state
+
+
+	for altitude in range(1, altitudes):
+		#Selects a row distribution in the transition matrix given current state
+		current_transition = (P_transition[current_state]).A1
+
+		#This chooses 0 or 1 depending on the distribution of P_transition
+		current_state = choice([0, 1], p = current_transition)
+		risk_at_altitudes[altitude] = 1 - current_state
+
+	return risk_at_altitudes
+
+# 25 is the number of simulations
+realizations = [realization() for _ in range(25)]
+average_risks = [sum(altitude_risk)/25 for altitude_risk in zip(*realizations)]
+
+#Plot
+plt.title("Realization risks given altitude")
+plt.xlabel("altitude"); plt.ylabel("realization")
+plt.imshow(realizations, cmap='pink', interpolation='nearest')
+plt.show()
+
+#Plot
+plt.title("Average realization risk given altitude")
+plt.xlabel("altitude"); plt.ylabel("Average")
+plt.bar(range(altitudes), average_risks)
+plt.show()
 
 #C
 """
-Legg till kommentar hær
+We place a sensor at altitude k and assume its status ("low" or "high").
+We then calculate the conditional probability.
 """
 #Vi antar at vi kjenner til om området k er høyrisiko/lavrisiko
-k = 20
+k = 20-1 #20. høydeparti er indeks 19 ved nullindeksering
 
-#Vi velger hvilket senario vi vil simulere.
-P_Xk = to_vector("1.00 0.00") #state 1 // low risk
-#P_Xk = to_vector("0.00 1.00") #state 2 // high risk
+altitude_risks_lowk=[P(X = altitude, low = k) for altitude in range(altitudes)]
+high_risks_lowk = [risk["high"] for risk in altitude_risks_lowk]
 
-#Vi gjør klar lista så vi kan legge til direkte istedenfor å bruke append().
-cond_altitude_risks = np.empty([altitudes, 2])
-cond_altitude_risks[k] = P_Xk
-for altitude in range(k+1, altitudes):
-	distance_from_k = altitude-k #always positive
-	P_Xlk = P_Xk @ P_transitions[distance_from_k]
-	cond_altitude_risks[altitude] = P_Xlk
+#Plot
+plt.title(f"High risk probability given low risk at {k}")
+plt.xlabel("altitude"); plt.ylabel("Probability")
+plt.bar(range(altitudes), high_risks_lowk)
+plt.show()
 
-for altitude in reversed(range(k)):
-	distance_from_k = k-altitude #always positive
+altitude_risks_highk=[P(X = altitude, high = k)for altitude in range(altitudes)]
+high_risks_highk = [risk["high"] for risk in altitude_risks_highk]
 
-	P_Xl = np.ravel(P_bottom @ P_transitions[altitude])
-	#Given P_Xk, l = (1,0) or l = (0,1), calculates P(X_k = P_Xk | X_k = l)
-	P_Xkl = [np.ravel(state @ P_transitions[distance_from_k]) for state in ([1,0],[0,1])]
-
-	P_Xlk = [
-		np.dot(P_Xk, P_Xkl[i])*P_Xl[i]/
-		(np.dot(P_Xk, P_Xkl[0])*P_Xl[0] + np.dot(P_Xk, P_Xkl[1])*P_Xl[1])
-	for i in range(2)]
-
-	cond_altitude_risks[altitude] = P_Xlk
-
-#plt.imshow(cond_altitude_risks.T, cmap='pink', interpolation='nearest')
-#plt.show()
+#Plot
+plt.title(f"High risk probability given high risk at {k}")
+plt.xlabel("altitude"); plt.ylabel("Probability")
+plt.bar(range(altitudes), high_risks_highk)
+plt.show()
 
 #D
 """
-Denne sier seg også litt selv; viss ikke: les oppgaveteksten.
+We calculate the minimal cost given that each high risk zone costs NOK5000.
 """
-total_cost = sum(5000 * high_risk for low_risk, high_risk in altitude_risks)
-#print(min(1E5, total_cost)) # 100000 < 158617.552552
+total_cost = 5000 * sum(risk["high"] for risk in altitude_risks)
+print(f"The minimum cost is {min(1E5, total_cost)}") # 100000 < 158617.552552
 
 #E
 """
-Legg till kommentar hær
+We calculate the expected cost, given that a sensor is telling us the
+state of altitude k.
 """
+
+#initialize empty list
 V = [0 for k in range(altitudes)]
-for k in range(altitudes):
-	for state in ([1,0],[0,1]):
-		P_Xk = state #This is not P_Xk | P_X0
-		cond_altitude_risks = np.empty([altitudes, 2])
-		cond_altitude_risks[k] = P_Xk
-		for altitude in range(k+1, altitudes):
-			distance_from_k = altitude-k #always positive
-			P_Xlk = P_Xk @ P_transitions[distance_from_k]
-			cond_altitude_risks[altitude] = P_Xlk
 
-		for altitude in reversed(range(k)):
-			"""P(Xk = 2 | Xl = 2) = 100%"""
-			distance_from_k = k-altitude #always positive
+for state in ("low", "high"):
+	for k in range(altitudes):
+		cost = 5000 * sum(P(X = alt, **{state: k})["high"] for alt in range(altitudes))
+		V[k] += min(1E5, cost) * P(X = k)[state]
 
-			P_Xl = np.ravel(P_bottom @ P_transitions[altitude])
-			#Given P_Xk, l = (1,0) or l = (0,1), calculates P(X_k = P_Xk | X_k = l)
-			P_Xkl = [np.ravel(state @ P_transitions[distance_from_k]) for state in ([1,0],[0,1])]
+print(f"Minimum value is {min(V)} at {V.index(min(V))}.")
 
-			P_Xlk = [
-				np.dot(P_Xk, P_Xkl[i])*P_Xl[i]/
-				(np.dot(P_Xk, P_Xkl[0])*P_Xl[0]+np.dot(P_Xk, P_Xkl[1])*P_Xl[1])
-			for i in range(2)]
-
-			cond_altitude_risks[altitude] = P_Xlk
-
-		k_sum = sum(5000 * high_risk for low_risk, high_risk in cond_altitude_risks)
-		V[k] += min(1E5, k_sum) * np.dot(np.ravel(P_bottom @ P_transitions[k]), state)
-
-#plt.plot(V)
-#print(f"Minimum value is {min(V)} at {V.index(min(V))}.")
-#plt.show()
+#Plot
+plt.title("Estimated cost given sensor at altitude k")
+plt.xlabel("sensor altitude"); plt.ylabel("estimated cost")
+plt.plot(V)
+plt.show()
